baf52b00676f4e9e9dc5c8460678739fbf9c607d
islam <eslamebrahim4045@gmail.com>
2024-02-28 00:42:14 +0200

    islam

---
+++ main1.js
@@ -0,0 +1,19 @@
+// Task 1
+// making function preview me my first and last name
+
+// inputs => firstName , lastName
+// algorithm => firstName+lastName
+// outputs   => firstName and lastName
+
+// inputs
+
+function myName(firstName, lastName) {
+  // algorithm
+
+  let fullName = firstName + " " + lastName;
+
+  // output
+
+  return fullName;
+}
+console.log(myName("islam", "ibrahim"));
