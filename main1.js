fdfc68a2eaad23023768c5aafb5ce66fb5f6732d
islam <eslamebrahim4045@gmail.com>
2024-02-28 01:49:31 +0200

    islam

--- main1.js
+++ main1.js
@@ -17,3 +17,62 @@ function myName(firstName, lastName) {
   return fullName;
 }
 console.log(myName("islam", "ibrahim"));
+
+
+
+
+
+
+
+
+
+task 2
+making a function to git the max number of array
+
+input
+let AllNumber = [50, 99, 150, 360, 1169, 5987, 9000];
+
+// algorithm
+
+// outputs => the max number of array
+
+function maxNumber(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+console.log(maxNumber(AllNumber));
+
+
+
+
+
+
+
+// task 3
+// making a function to git the min number of array
+
+let studentNumbers = [50,250,60,70,99,34,164];
+
+function minName ( arr) {
+
+let min = arr[0];
+for ( let i =1  ; i < arr.length ; i++  ){
+
+if ( arr[i] < min){
+
+    min = arr[i];
+}
+
+}
+return min
+
+
+
+}
+console.log( minName(studentNumbers));
+
