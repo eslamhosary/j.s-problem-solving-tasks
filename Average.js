37f8c91c8fa804ea85f7397eb0f88bdb7fb3acb3
islam <eslamebrahim4045@gmail.com>
2024-03-03 07:10:07 +0200

    arr.reduce

--- Average.js
+++ Average.js
@@ -103,48 +103,102 @@ const customer = [
   {
     id: 1,
     name: "Ahmed",
-    score: 20,
-    target: 55,
+    score: 4,
+    target: 2,
   },
   {
     id: 2,
     name: "Mazero",
-    score: 90,
-    target: 77,
+    score: 4,
+    target: 2,
   },
   {
     id: 3,
     name: "Elham",
-    score: 100,
-    target: 90,
+    score: 4,
+    target: 2,
   },
   {
     id: 4,
     name: "Osama",
-    score: 70,
-    target: 60,
+    score: 4,
+    target: 2,
   },
   {
     id: 5,
     name: "Gamal",
-    score: 85,
-    target: 30,
+    score: 4,
+    target: 2,
   },
   {
     id: 6,
     name: "islam",
-    score: 95,
-    target: 80,
+    score: 4,
+    target: 2,
   },
 ];
 
-let customerNumbers = customer.length;
+// let customerNumbers = customer.length;
 
-let totalDegree = customer.reduce(function (num1, num2) {
-  let finalScore = num1 + num2.score + num2.target;
+// let totalDegree = customer.reduce(function (num1, num2) {
+//   let finalScore = num1 + num2.score + num2.target;
 //   console.log(finalScore);
-  return finalScore;
-}, 0);
+//   return finalScore;
+// }, 0);
 
-let Average = totalDegree / customerNumbers;
-console.log(`${Average}%`);
+// // let Average = totalDegree / customerNumbers;
+// // console.log(`${Average}%`);
+
+
+
+
+ let partner  = [
+    {
+      id: 1,
+      name: "Ahmed",
+      score: 40,
+      target: 75,
+    },
+    {
+      id: 2,
+      name: "Mazero",
+      score: 90,
+      target: 80,
+    },
+    {
+      id: 3,
+      name: "Elham",
+      score: 45,
+      target: 90,
+    },
+    {
+      id: 4,
+      name: "Osama",
+      score: 70,
+      target: 60,
+    },
+    {
+      id: 5,
+      name: "Gamal",
+      score: 50,
+      target: 70,
+    },
+    {
+      id: 6,
+      name: "islam",
+      score: 95,
+      target: 80,
+    },
+  ];
+
+  let number = partner.length
+//   console.log(number);
+
+let totalScore = partner.reduce(function(prev , curr){
+    let finalTotal = prev + curr.score + curr.target ;
+    return finalTotal
+
+} ,0)
+
+let average = totalScore / number
+console.log(`${average}%`);
